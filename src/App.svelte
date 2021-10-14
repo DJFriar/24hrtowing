d14f36fc6c0a736c656e65e420d004f97200faf6
DJFriar <tommy.craft@icloud.com>
2021-10-14 18:53:57 -0500

    initial layout idea

---
+++ src/App.svelte
@@ -0,0 +1,65 @@
+<script>
+	export let phoneNum;
+  export let email;
+</script>
+
+<main>
+  <img src="/24HourTowing-Logo.png" alt="24 Hour Towing Logo">
+  <h3>Serving the DFW Metroplex and surrounding areas.</h3>
+	<h1>{phoneNum}</h1>
+  <h2><a href="mailto:{email}">{email}</a></h2>
+</main>
+
+<footer>
+  Site created & hosted by <a href="https://www.ambitiousnerds.com" target="_blank">ambitiousNerds</a>
+</footer>
+
+<style>
+	main {
+    background-color: #282828;
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+  img {
+    max-width: 400px;
+  }
+
+	h1 {
+		color: #07FC07;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 200;
+	}
+
+  h2 {
+		color: #07FC07;
+		font-size: 2em;
+		font-weight: 200;
+	}
+
+  h3 {
+		color: #07FC07;
+		font-size: 1em;
+		font-weight: 200;
+	}
+
+  a:active, a:link, a:visited {
+    color: #07FC07;
+  }
+
+  footer {
+    background-color: #282828;
+    text-align: center;
+    color: #07FC07;
+    font-size: .8em;
+  }
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
