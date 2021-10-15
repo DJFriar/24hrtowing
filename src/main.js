f384f2c0f533d23f900b3444cd0b57d6a0068b81
DJFriar <tommy.craft@icloud.com>
2021-10-14 20:29:59 -0500

    corrected email address

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const app = new App({
 	target: document.body,
 	props: {
 		phoneNum: '(469) 669-3TOW',
-    email: 'johnweiss@24hrtowingllc.com'
+    email: 'johnweiss@24hrtowingtexas.com'
 	}
 });
 
