6a6c90e502c710889913847c282b0546842edfe1
DJFriar <tommy.craft@icloud.com>
2021-10-14 19:14:29 -0500

    update port number

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
-		phoneNum: '(469) 669-3869',
+		phoneNum: '(469) 669-3TOW',
     email: 'johnweiss@24hrtowingllc.com'
 	}
 });
